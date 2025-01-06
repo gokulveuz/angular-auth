5afbbdd0d76194a849fa6af02f2320374ab332a0
websoulgokulks@gmail.com <gokul>
2025-01-06 19:09:35 +0530

    authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), [ withComponentInputBinding(),provideClientHydration()]],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),provideAnimations(),provideHttpClient()]
 };
+
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,55 @@
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor(private LoginService:LoginService)
+  inValid =false;
+  warningMsg = '';
+
+  constructor(private LoginService:LoginService,private fb: FormBuilder)
   {
 
   }
+
+  ngOnInit()
+  {
+    this.initFormGroup();
+  }
+
+  initFormGroup(): void {
+    this.loginForm = this.fb.group({
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]),
+      remember_me: new FormControl('false')
+    });
+  }
+
+  submit()
+  {
+    console.log('loginform',this.loginForm);
+
+  }
+
+  onchange(env: any) {
+    const len = String(env.target.value);
+
+    if (len.length > 0) {
+      this.inValid = false;
+      this.warningMsg = '';
+    }
+
+  }
+
+
 }
