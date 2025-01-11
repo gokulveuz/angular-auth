a6692644d196cbb3d3b2a36c08c03b595c8f4a1b
websoulgokulks@gmail.com <gokul>
2025-01-11 18:47:52 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,18 +2,18 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, 
 import { LoginService } from './login.service';
 import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,FormsModule],
+  imports: [ReactiveFormsModule,FormsModule,NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-
+  isRememberMe = false;
   inValid =false;
   warningMsg = '';
 
@@ -37,8 +37,16 @@ export class LoginComponent implements OnInit {
 
   submit()
   {
+    if (this.loginForm.invalid) {
+      this.inValid = true;
+      if (this.loginForm.controls['username'].value == '') {
+        this.warningMsg = "User name is required";
+      } else if (this.loginForm.controls['password'].value == '') {
+        this.warningMsg = "Password is required";
+      }
+      return;
+    }
     console.log('loginform',this.loginForm);
-
   }
 
   onchange(env: any) {
@@ -51,5 +59,35 @@ export class LoginComponent implements OnInit {
 
   }
 
+  getFormValue(name: string) {
+    return this.loginForm.get(name)?.value;
+  }
 
+
+  createLoginBody(): {
+    username:String,
+    password:String,
+    recpatcha:String
+  } {
+    return {
+      username: this.getFormValue('username'),
+      password: this.getFormValue('password'),
+      recpatcha:this.getFormValue('remember_me')
+    };
+  }
+
+
+
+  enterKey(event: any, number: number) {
+    const password = document.getElementById("password");
+    if (event.keyCode === 13 && event.target.value !== "") {
+      if (number == 1) {
+        password?.focus();
+      }
+      else {
+        password?.blur();
+        this.submit();
+      }
+    }
+  }
 }
